45b970b3854c872fbefaf458b046bb800fe1d8f7
xingchen-FL <37186195+xingchen-FL@users.noreply.github.com>
2024-08-21 17:34:36 +0800

    搭建框架，引入ui 依赖

---
+++ src/main/java/com/demo/detailprocess/DetailProcessApplication.java
@@ -0,0 +1,13 @@
+package com.demo.detailprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetailProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetailProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/detailprocess/DetailProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.detailprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetailProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
